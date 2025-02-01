e8641f52ac80c95b879d3d3201e5cd9bd2d3572e
Slavi Sokolov <133207921+TripleS01@users.noreply.github.com>
2025-02-01 14:49:57 +0200

    task model, auth routes

--- server/src/routes/authRoutes.js
+++ server/src/routes/authRoutes.js
@@ -1,13 +1,15 @@
 import express from 'express';
 
-import { login, logout, register } from '../controllers/authController.js';
+import {
+    registerUser,
+    loginUser,
+    logoutUser,
+} from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post('/register', register);
-
-router.post('/login', login);
-
-router.post('/logout', logout);
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
 
 export default router;
