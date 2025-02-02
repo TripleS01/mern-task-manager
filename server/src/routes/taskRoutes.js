c8eac91ac37667223b918908340721cef8eb4db9
Slavi Sokolov <133207921+TripleS01@users.noreply.github.com>
2025-02-02 19:28:18 +0200

    routes

---
+++ server/src/routes/taskRoutes.js
@@ -0,0 +1,19 @@
+import express from "express";
+import {
+    createTask,
+    deleteTask,
+    getTask,
+    getTasks,
+    updateTask,
+} from "../controllers/taskController.js";
+import { authMiddleware } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/task/create", authMiddleware, createTask);
+router.get("/tasks", authMiddleware, getTasks);
+router.get("/task/:id", authMiddleware, getTask);
+router.patch("/task/:id", authMiddleware, updateTask);
+router.delete("/task/:id", authMiddleware, deleteTask);
+
+export default router;
---
+++ server/src/routes/userRoutes.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import { authMiddleware } from "../middleware/authMiddleware.js";
+import {
+    getUser,
+    updateUser,
+} from "../controllers/userController.js";
+
+const router = express.Router();
+
+router.get("/user", authMiddleware, getUser);
+router.patch("/user", authMiddleware, updateUser);
+
+export default router;
